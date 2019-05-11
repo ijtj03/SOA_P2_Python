033555ccff5229cb8643187ba986bc3bd2e3ea77
ijtj03 <ijtj03@estudiantec.cr>
2019-05-11 04:31:20 -0600

    cambios

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.debug = True
 
 app.add_url_rule(
-    '/graphql',
-    view_func=GraphQLView.as_view('productos', schema=schema, graphiql=True)
+    '/productos',
+    view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
 )
 
 if __name__ == '__main__':
